92e8e880192f8fe3cc6151a36f1e4c1e9ace2f89
quickfix <devops.july2.2017@gmail.com>
2019-03-05 18:09:13 +0530

    Update awxhost-setup.sh

--- awxhost-setup.sh
+++ awxhost-setup.sh
@@ -8,7 +8,7 @@ sudo apt-get update && \
 sudo apt-get install \
   apt-transport-https \
   ca-certificates \
-  curl \&& \
+  curl \
   software-properties-common && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
 sudo apt-key fingerprint 0EBFCD88 && \
